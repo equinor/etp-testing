0ba06cffe94d1d1e9b729dcab2d508d1fa48b7b3
Øyvind Sigmundson Schøyen <oyvind.schoyen@bluware.com>
2023-11-30 15:48:19 +0100

    Continue

--- talk_to_open_etp_server.py
+++ talk_to_open_etp_server.py
@@ -1,9 +1,14 @@
+import uuid
 import time
 import asyncio
-import websockets
 import json
 import datetime
 import warnings
+import pprint
+
+import httpx
+import websockets
+import fastavro
 
 
 def get_token(token_filename: str = ".token") -> str:
@@ -31,7 +36,18 @@ async def hello_azurehost(token: str):
         print(await ping)
 
 
-# asyncio.run(hello_azurehost(get_token()))
+def get_azurehost_capabilities(token: str):
+    # TODO: Why doesn't this work???
+    url = "https://interop-rddms.azure-api.net/.well-known/etp-server-capabilities?GetVersion=etp12.energistics.org"
+    headers = {"Authorization": "Bearer " + token}
+    return httpx.get(url, headers=headers).raise_for_status().json()
+
+
+# token = get_token()
+# pprint.pprint(get_azurehost_capabilities(token))
+#
+# asyncio.run(hello_azurehost(token))
+# wat
 
 
 async def hello_localhost():
@@ -45,9 +61,54 @@ async def hello_localhost():
         print(await ping)
 
 
-with open("etp.avpr", "r") as f:
-    avro_schemas = json.load(f)
+def get_localhost_capabilities():
+    url = "http://localhost:9002/.well-known/etp-server-capabilities?GetVersion=etp12.energistics.org"
+    return httpx.get(url).raise_for_status().json()
 
-print(avro_schemas)
+
+# pprint.pprint(get_localhost_capabilities())
+# wat
+
+
+with open("etp.avpr", "r") as foo:
+    jschema = json.load(foo)
+
+
+def parse_func(js, named_schemas=dict()):
+    ps = fastavro.schema.parse_schema(js, named_schemas)
+    return fastavro.schema.fullname(ps), ps
+
+
+etp_schemas = dict(parse_func(js) for js in jschema["types"])
+
+mh_record = dict(
+    protocol=0,  # Core protocol
+    messageType=1,  # RequestSession
+    correlationId=0,  # Ignored for RequestSession
+    messageId=2,  # non-zero, even numbers for the client
+    messageFlags=0x2,  # FIN-bit
+)
+
+rs_record = dict(
+    applicationName="oycli",
+    applicationVersion="0.0.1",
+    clientInstanceId=uuid.uuid4().bytes,
+    requestedProtocols=[
+        dict(  # SupportedProtocol
+            protocol=3,
+            protocolVersion=dict(
+                major=1,
+                minor=2,
+            ),
+            role="store",
+        ),
+    ],
+    supportedDataObjects=[
+        dict(  # SupportedDataObject
+            qualifiedType="resqml20.*",
+        ),
+    ],
+    # currentDateTime=
+)
 
 asyncio.run(hello_localhost())
