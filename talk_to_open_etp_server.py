48186966fa288543f037328e9102e49c6db0bddc
Øyvind Sigmundson Schøyen <oyvind.schoyen@bluware.com>
2023-12-01 18:00:40 +0100

    Make good progress!

--- talk_to_open_etp_server.py
+++ talk_to_open_etp_server.py
@@ -1,3 +1,4 @@
+import io
 import uuid
 import time
 import asyncio
@@ -61,6 +62,17 @@ async def hello_localhost():
         print(await ping)
 
 
+async def query_localhost(message):
+    uri = "ws://localhost:9002"
+    async with websockets.connect(
+        uri,
+        subprotocols=["etp12.energistics.org"],
+    ) as ws:
+        print(ws.open)
+        await ws.send(message)
+        return await ws.recv()
+
+
 def get_localhost_capabilities():
     url = "http://localhost:9002/.well-known/etp-server-capabilities?GetVersion=etp12.energistics.org"
     return httpx.get(url).raise_for_status().json()
@@ -81,34 +93,372 @@ def parse_func(js, named_schemas=dict()):
 
 etp_schemas = dict(parse_func(js) for js in jschema["types"])
 
-mh_record = dict(
-    protocol=0,  # Core protocol
-    messageType=1,  # RequestSession
-    correlationId=0,  # Ignored for RequestSession
-    messageId=2,  # non-zero, even numbers for the client
-    messageFlags=0x2,  # FIN-bit
-)
 
-rs_record = dict(
-    applicationName="oycli",
-    applicationVersion="0.0.1",
-    clientInstanceId=uuid.uuid4().bytes,
-    requestedProtocols=[
-        dict(  # SupportedProtocol
-            protocol=3,
-            protocolVersion=dict(
-                major=1,
-                minor=2,
+# mh_record = dict(
+#     protocol=0,  # Core protocol
+#     messageType=1,  # RequestSession
+#     correlationId=0,  # Ignored for RequestSession
+#     messageId=2,  # non-zero, even numbers for the client
+#     messageFlags=0x2,  # FIN-bit
+# )
+#
+# rs_record = dict(
+#     applicationName="oycli",
+#     applicationVersion="0.0.1",
+#     clientInstanceId=uuid.uuid4().bytes,
+#     requestedProtocols=[
+#         dict(  # SupportedProtocol
+#             protocol=3,
+#             protocolVersion=dict(
+#                 major=1,
+#                 minor=2,
+#             ),
+#             role="store",
+#         ),
+#     ],
+#     supportedDataObjects=[
+#         dict(  # SupportedDataObject
+#             qualifiedType="resqml20.*",
+#         ),
+#     ],
+#     currentDateTime=datetime.datetime.now(datetime.timezone.utc).timestamp(),
+#     earliestRetainedChangeTime=0,
+#     serverAuthorizationRequired=False,
+# )
+#
+# asyncio.run(hello_localhost())
+#
+# fo = io.BytesIO()
+# # Write the message header
+# fastavro.write.schemaless_writer(fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record)
+# # Write the request session body
+# fastavro.write.schemaless_writer(fo, etp_schemas["Energistics.Etp.v12.Protocol.Core.RequestSession"], rs_record)
+#
+# resp = asyncio.run(query_localhost(fo.getvalue()))
+#
+# fo = io.BytesIO(resp)
+# record = fastavro.read.schemaless_reader(
+#     fo,
+#     etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+#     return_record_name=True,
+# )
+# pprint.pprint(record)
+# record = fastavro.read.schemaless_reader(
+#     fo,
+#     etp_schemas["Energistics.Etp.v12.Protocol.Core.OpenSession"],
+#     return_record_name=True,
+# )
+# pprint.pprint(record)
+
+
+async def start_and_stop(url="ws://localhost:9002", headers={}):
+    print(f"Talking to: {url}")
+    async with websockets.connect(
+        url,
+        extra_headers=headers,
+        subprotocols=["etp12.energistics.org"],
+    ) as ws:
+        MSG_ID = 2
+
+        # TODO: Figure out how set websocket max sizes
+        # Request session, i.e., start the thingy
+        mh_record = dict(
+            protocol=0,  # Core protocol
+            messageType=1,  # RequestSession
+            correlationId=0,  # Ignored for RequestSession
+            messageId=(MSG_ID := MSG_ID + 2),  # non-zero, even numbers for the client
+            messageFlags=0x2,  # FIN-bit
+        )
+
+        rs_record = dict(
+            applicationName="oycli",
+            applicationVersion="0.0.1",
+            clientInstanceId=uuid.uuid4().bytes,
+            requestedProtocols=[  # [SupportedProtocol]
+                dict(
+                    protocol=3,  # Discovery
+                    protocolVersion=dict(
+                        major=1,
+                        minor=2,
+                    ),
+                    role="store",
+                ),
+                dict(
+                    protocol=9,  # DataArray
+                    protocolVersion=dict(
+                        major=1,
+                        minor=2,
+                    ),
+                    role="store",
+                ),
+                dict(
+                    protocol=24,  # Dataspace
+                    protocolVersion=dict(
+                        major=1,
+                        minor=2,
+                    ),
+                    role="store",
+                ),
+            ],
+            supportedDataObjects=[
+                dict(  # SupportedDataObject
+                    qualifiedType="resqml20.*",
+                ),
+            ],
+            currentDateTime=datetime.datetime.now(datetime.timezone.utc).timestamp(),
+            earliestRetainedChangeTime=0,
+            serverAuthorizationRequired=False,
+        )
+
+        fo = io.BytesIO()
+        # Write the message header
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
+        )
+        # Write the request session body
+        fastavro.write.schemaless_writer(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Protocol.Core.RequestSession"],
+            rs_record,
+        )
+
+        await ws.send(fo.getvalue())
+        resp = await ws.recv()
+
+        fo = io.BytesIO(resp)
+        record = fastavro.read.schemaless_reader(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+            return_record_name=True,
+        )
+        pprint.pprint(record)
+        record = fastavro.read.schemaless_reader(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Protocol.Core.OpenSession"],
+            return_record_name=True,
+        )
+        pprint.pprint(record)
+
+        # Query dataspaces
+        mh_record = dict(
+            protocol=24,  # Dataspace
+            messageType=1,  # GetDataspaces
+            correlationId=0,  # Ignored
+            messageId=(MSG_ID := MSG_ID + 2),
+            messageFlags=0x2,  # Fin
+        )
+        gd_record = dict(
+            storeLastWriteFilter=None,  # Include all dataspaces
+        )
+
+        fo = io.BytesIO()
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
+        )
+        fastavro.write.schemaless_writer(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Protocol.Dataspace.GetDataspaces"],
+            gd_record,
+        )
+
+        # The final response message should have the fin-bit set in the message header
+        await ws.send(fo.getvalue())
+
+        while True:
+            resp = await ws.recv()
+
+            fo = io.BytesIO(resp)
+            mh_record = fastavro.read.schemaless_reader(
+                fo,
+                etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+                return_record_name=True,
+            )
+            pprint.pprint(mh_record)
+
+            if mh_record["messageType"] == 1000:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas["Energistics.Etp.v12.Protocol.Core.ProtocolException"],
+                    return_record_name=True,
+                )
+            else:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas[
+                        "Energistics.Etp.v12.Protocol.Dataspace.GetDataspacesResponse"
+                    ],
+                    return_record_name=True,
+                )
+            pprint.pprint(record)
+
+            if (mh_record["messageFlags"] & 0x2) != 0:
+                # We have received a FIN-bit, i.e., the last reponse has been
+                # read.
+                print("Last message read from this protocol")
+                break
+
+        # Get resources
+        mh_record = dict(
+            protocol=3,  # Store
+            messageType=1,  # GetResources
+            correlationId=0,  # Ignored
+            messageId=(MSG_ID := MSG_ID + 2),
+            messageFlags=0x2,
+        )
+        gr_record = dict(  # GetResources
+            context=dict(  # ContextInfo
+                uri="eml:///dataspace('demo/pss-rand')",
+                depth=2,
+                navigableEdges="Primary",
             ),
-            role="store",
-        ),
-    ],
-    supportedDataObjects=[
-        dict(  # SupportedDataObject
-            qualifiedType="resqml20.*",
-        ),
-    ],
-    # currentDateTime=
-)
+            scope="targets",
+            storeLastWriteFilter=None,
+            activeStatusFilter=None,  # perhaps active?
+        )
 
-asyncio.run(hello_localhost())
+        fo = io.BytesIO()
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
+        )
+        fastavro.write.schemaless_writer(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Protocol.Discovery.GetResources"],
+            gr_record,
+        )
+
+        await ws.send(fo.getvalue())
+
+        while True:
+            resp = await ws.recv()
+
+            fo = io.BytesIO(resp)
+            mh_record = fastavro.read.schemaless_reader(
+                fo,
+                etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+                return_record_name=True,
+            )
+            pprint.pprint(mh_record)
+
+            if mh_record["messageType"] == 1000:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas["Energistics.Etp.v12.Protocol.Core.ProtocolException"],
+                    return_record_name=True,
+                )
+            else:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas[
+                        "Energistics.Etp.v12.Protocol.Discovery.GetResourcesResponse"
+                    ],
+                )
+            pprint.pprint(record)
+
+            if (mh_record["messageFlags"] & 0x2) != 0:
+                # We have received a FIN-bit, i.e., the last reponse has been
+                # read.
+                print("Last message read from this protocol")
+                break
+
+        # Get data array metadata
+        # TODO: Figure out how to do more sophisticated querying
+        # TODO: You need to figure out how to find the pathInResource!!!
+        mh_record = dict(
+            protocol=9,  # DataArray
+            messageType=6,  # GetDataArrayMetadata
+            correlationId=0,  # Ignored
+            messageId=(MSG_ID := MSG_ID + 2),
+            messageFlags=0x2,  # Multi-part=False
+        )
+        gdam_record = dict(
+            dataArrays=dict(
+                zero=dict(
+                    uri="eml:///dataspace('demo/pss-rand')/eml20.obj_EpcExternalPartReference(49e4c1de-891c-11ee-a655-4da4d0f45a8a)",
+                    # TODO: Figure out how get this path
+                    pathInResource="RESQML/49e4c1de-891c-11ee-a655-4da4d0f45a8a",
+                ),
+            ),
+        )
+
+        fo = io.BytesIO()
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
+        )
+        fastavro.write.schemaless_writer(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Protocol.DataArray.GetDataArrayMetadata"],
+            gdam_record,
+        )
+
+        await ws.send(fo.getvalue())
+
+        while True:
+            resp = await ws.recv()
+
+            fo = io.BytesIO(resp)
+            mh_record = fastavro.read.schemaless_reader(
+                fo,
+                etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+                return_record_name=True,
+            )
+            pprint.pprint(mh_record)
+
+            if mh_record["messageType"] == 1000:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas["Energistics.Etp.v12.Protocol.Core.ProtocolException"],
+                    return_record_name=True,
+                )
+            else:
+                record = fastavro.read.schemaless_reader(
+                    fo,
+                    etp_schemas[
+                        "Energistics.Etp.v12.Protocol.DataArray.GetDataArrayMetadataResponse"
+                    ],
+                )
+            pprint.pprint(record)
+
+            if (mh_record["messageFlags"] & 0x2) != 0:
+                # We have received a FIN-bit, i.e., the last reponse has been
+                # read.
+                print("Last message read from this protocol")
+                break
+
+        # Close session
+        mh_record = dict(
+            protocol=0,  # Core
+            messageType=5,  # CloseSession
+            correlationId=0,  # Ignored
+            messageId=(MSG_ID := MSG_ID + 2),
+            messageFlags=0x2,
+        )
+
+        cs_record = dict(
+            reason="Because I said so",
+        )
+
+        fo = io.BytesIO()
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
+        )
+        fastavro.write.schemaless_writer(
+            fo, etp_schemas["Energistics.Etp.v12.Protocol.Core.CloseSession"], cs_record
+        )
+
+        await ws.send(fo.getvalue())
+
+        await ws.wait_closed()
+        assert ws.closed
+        print(ws.close_reason)
+
+
+# TODO: Populate localhost with data
+# asyncio.run(start_and_stop())
+
+
+asyncio.run(
+    start_and_stop(
+        url="wss://interop-rddms.azure-api.net",
+        headers={"Authorization": "Bearer " + get_token()},
+    ),
+)
