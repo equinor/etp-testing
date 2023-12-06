f3f469b8c34f0d23337bef3fcb63eea3d322f905
Øyvind Sigmundson Schøyen <oyvind.schoyen@bluware.com>
2023-12-06 16:44:56 +0100

    Start on some refactoring and put/delete

--- talk_to_open_etp_server.py
+++ talk_to_open_etp_server.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import io
 import uuid
@@ -9,11 +10,14 @@ import warnings
 import pprint
 import enum
 
+# Third-party imports
 import lxml.etree as ET
 import httpx
 import websockets
 import fastavro
 import numpy as np
+import resqpy
+import resqpy.model
 
 
 def get_token(token_filename: str = ".token") -> str:
@@ -76,47 +80,224 @@ etp_schemas = dict(parse_func(js) for js in jschema["types"])
 
 
 class MHFlags(enum.Enum):
+    # Flags in MessageHeader, see section 23.25 in the ETP 1.2 standard
     FIN = 0x2
     COMPRESSED = 0x8
     ACK = 0x10
     HEADER_EXTENSION = 0x20
 
 
-class MessageID:
-    def __init__(self, client=True):
-        # Client-side should use unique, increasing, positive, even message id's.
-        self.message_id = 2 if client else 1
-        self.lock = asyncio.Lock()
+class ClientMessageId:
+    lock = asyncio.Lock()
+    # Unique, positive, increasing, even integers for the client
+    message_id = 2
 
-    async def __call__(self):
-        async with self.lock:
-            ret_id = self.message_id
-            self.message_id += 2
+    @staticmethod
+    async def get_next_id():
+        async with ClientMessageId.lock:
+            ret_id = ClientMessageId.message_id
+            ClientMessageId.message_id += 2
             return ret_id
 
 
-async def start_and_stop(url="ws://localhost:9002", headers={}):
+def serialize_message(header_record, body_record, body_schema_key):
+    # TODO: Possibly support compression?
+    fo = io.BytesIO()
+    fastavro.write.schemaless_writer(
+        fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], header_record
+    )
+    fastavro.write.schemaless_writer(fo, etp_schemas[body_schema_key], body_record)
+
+    return fo.getvalue()
+
+
+async def close_session(ws, reason):
+    # Close session
+    mh_record = dict(
+        protocol=0,  # Core
+        messageType=5,  # CloseSession
+        correlationId=0,  # Ignored
+        messageId=await ClientMessageId.get_next_id(),
+        messageFlags=MHFlags.FIN.value,
+    )
+
+    cs_record = dict(
+        reason=reason,
+    )
+
+    await ws.send(
+        serialize_message(
+            mh_record, cs_record, "Energistics.Etp.v12.Protocol.Core.CloseSession"
+        )
+    )
+
+    await ws.wait_closed()
+    assert ws.closed
+    print(f"Websocket close reason: {ws.close_reason}")
+
+
+async def handle_multipart_response(ws, schema_key):
+    # Note that we only handle ProtocolException errors for now
+    records = []
+    while True:
+        response = await ws.recv()
+
+        fo = io.BytesIO(response)
+        mh_record = fastavro.read.schemaless_reader(
+            fo,
+            etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
+            return_record_name=True,
+        )
+
+        if mh_record["messageType"] == 1000:
+            # ProtocolException
+            record = fastavro.read.schemaless_reader(
+                fo,
+                etp_schemas["Energistics.Etp.v12.Protocol.Core.ProtocolException"],
+                return_record_name=True,
+            )
+            # Output error object
+            pprint.pprint(record)
+            await close_session(ws, reason=f"Error from protocol '{schema_key}'")
+            sys.exit(1)
+        else:
+            record = fastavro.read.schemaless_reader(
+                fo,
+                etp_schemas[schema_key],
+                return_record_name=True,
+            )
+            records.append(record)
+
+        if (mh_record["messageFlags"] & MHFlags.FIN.value) != 0:
+            # We have received a FIN-bit, i.e., the last reponse has been
+            # read.
+            break
+
+    return records
+
+
+async def get_dataspaces(ws, store_last_write_filter=None):
+    # Query dataspaces
+    mh_record = dict(
+        protocol=24,  # Dataspace
+        messageType=1,  # GetDataspaces
+        correlationId=0,  # Ignored
+        messageId=await ClientMessageId.get_next_id(),
+        messageFlags=MHFlags.FIN.value,  # Fin
+    )
+    gd_record = dict(
+        storeLastWriteFilter=None,  # Include all dataspaces
+    )
+
+    # The final response message should have the fin-bit set in the message header
+    await ws.send(
+        serialize_message(
+            mh_record, gd_record, "Energistics.Etp.v12.Protocol.Dataspace.GetDataspaces"
+        )
+    )
+
+    return await handle_multipart_response(
+        ws, "Energistics.Etp.v12.Protocol.Dataspace.GetDataspacesResponse"
+    )
+
+
+async def put_dataspaces(ws, dataspaces):
+    uris = list(
+        map(lambda x: (x if "eml:///" in x else f"eml:///dataspace('{x}')"), dataspaces)
+    )
+    paths = list(
+        map(lambda x: re.search(r"eml:///dataspace\('(.+)'\)", x).group(1), uris)
+    )
+
+    mh_record = dict(
+        protocol=24,  # Dataspace
+        messageType=3,  # PutDataspaces
+        correlationId=0,  # Ignored
+        messageId=await ClientMessageId.get_next_id(),
+        messageFlags=MHFlags.FIN.value,
+    )
+    time = datetime.datetime.now(datetime.timezone.utc).timestamp()
+    pds_record = dict(
+        dataspaces=dict(
+            (
+                uri,
+                dict(
+                    uri=uri,
+                    path=path,
+                    # Here we create the dataspace for the first time, hence last write
+                    # and created are the same
+                    storeLastWrite=time,
+                    storeCreated=time,
+                ),
+            )
+            for uri, path in zip(uris, paths)
+        )
+    )
+    pprint.pprint(pds_record)
+
+    await ws.send(
+        serialize_message(
+            mh_record,
+            pds_record,
+            "Energistics.Etp.v12.Protocol.Dataspace.PutDataspaces",
+        )
+    )
+
+    return await handle_multipart_response(
+        ws,
+        "Energistics.Etp.v12.Protocol.Dataspace.PutDataspacesResponse",
+    )
+
+
+async def delete_dataspaces(ws, dataspaces):
+    uris = list(
+        map(lambda x: (x if "eml:///" in x else f"eml:///dataspace('{x}')"), dataspaces)
+    )
+
+    mh_record = dict(
+        protocol=24,  # Dataspace
+        messageType=4,  # DeleteDataspaces
+        correlationId=0,  # Ignored
+        messageId=await ClientMessageId.get_next_id(),
+        messageFlags=MHFlags.FIN.value,
+    )
+    dds_record = dict(uris=dict((uri, uri) for uri in uris))
+
+    await ws.send(
+        serialize_message(
+            mh_record,
+            dds_record,
+            "Energistics.Etp.v12.Protocol.Dataspace.DeleteDataspaces",
+        )
+    )
+
+    return await handle_multipart_response(
+        ws, "Energistics.Etp.v12.Protocol.Dataspace.DeleteDataspacesResponse"
+    )
+
+
+async def start_and_stop(
+    url="ws://localhost:9002", headers={}, dataspace="demo/rand-cli", verify=True
+):
     print(f"Talking to: {url}")
     async with websockets.connect(
         url,
         extra_headers=headers,
         subprotocols=["etp12.energistics.org"],
     ) as ws:
-        msg_id = MessageID(client=True)
-
         # TODO: Figure out how to set websocket max sizes
         # Request session, i.e., start the thingy
         mh_record = dict(
             protocol=0,  # Core protocol
             messageType=1,  # RequestSession
             correlationId=0,  # Ignored for RequestSession
-            messageId=await msg_id(),  # non-zero, even numbers for the client
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # FIN-bit
         )
 
         rs_record = dict(
-            applicationName="oycli",
-            applicationVersion="0.0.1",
+            applicationName="somecli",
+            applicationVersion="0.0.0",
             clientInstanceId=uuid.uuid4().bytes,
             requestedProtocols=[  # [SupportedProtocol]
                 dict(
@@ -192,75 +373,72 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             etp_schemas["Energistics.Etp.v12.Protocol.Core.OpenSession"],
             return_record_name=True,
         )
-        pprint.pprint(record)
 
-        # Query dataspaces
-        mh_record = dict(
-            protocol=24,  # Dataspace
-            messageType=1,  # GetDataspaces
-            correlationId=0,  # Ignored
-            messageId=await msg_id(),
-            messageFlags=MHFlags.FIN.value,  # Fin
-        )
-        gd_record = dict(
-            storeLastWriteFilter=None,  # Include all dataspaces
-        )
+        records = await get_dataspaces(ws, store_last_write_filter=None)
 
-        fo = io.BytesIO()
-        fastavro.write.schemaless_writer(
-            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
-        )
-        fastavro.write.schemaless_writer(
-            fo,
-            etp_schemas["Energistics.Etp.v12.Protocol.Dataspace.GetDataspaces"],
-            gd_record,
-        )
+        if any(
+            ds["path"] == dataspace for record in records for ds in record["dataspaces"]
+        ):
+            print(f"Deleting dataspace {dataspace}")
+            records = await delete_dataspaces(ws, [dataspace])
 
-        # The final response message should have the fin-bit set in the message header
-        await ws.send(fo.getvalue())
+            if verify:
+                # Test to see that the dataspace really was deleted
+                records = await get_dataspaces(ws, store_last_write_filter=None)
+                assert not any(
+                    ds["path"] == dataspace
+                    for record in records
+                    for ds in record["dataspaces"]
+                )
 
-        records = []
-        while True:
-            resp = await ws.recv()
-
-            fo = io.BytesIO(resp)
-            mh_record = fastavro.read.schemaless_reader(
-                fo,
-                etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"],
-                return_record_name=True,
+        # Now 'dataspace' does not exist
+        # Next, we create it
+        records = await put_dataspaces(ws, [dataspace])
+        if verify:
+            # Test to see that the dataspace now exists
+            records = await get_dataspaces(ws, store_last_write_filter=None)
+            assert any(
+                ds["path"] == dataspace
+                for record in records
+                for ds in record["dataspaces"]
             )
-            pprint.pprint(mh_record)
 
-            if mh_record["messageType"] == 1000:
-                record = fastavro.read.schemaless_reader(
-                    fo,
-                    etp_schemas["Energistics.Etp.v12.Protocol.Core.ProtocolException"],
-                    return_record_name=True,
-                )
-                pprint.pprint(record)
-                sys.exit(1)
-            else:
-                record = fastavro.read.schemaless_reader(
-                    fo,
-                    etp_schemas[
-                        "Energistics.Etp.v12.Protocol.Dataspace.GetDataspacesResponse"
-                    ],
-                    return_record_name=True,
-                )
-                records.append(record)
+        # Create random test data
+        model = resqpy.model.new_model("test.epc", quiet=False)
+        # This needs to be done in order to get a uuid
+        crs = resqpy.crs.Crs(model)
+        crs.create_xml()
+        z_values = np.random.random((3, 4))
+        mesh = resqpy.surface.Mesh(
+            model,
+            crs_uuid=model.crs_uuid,
+            mesh_flavour="reg&z",
+            ni=z_values.shape[1],  # rows
+            nj=z_values.shape[0],  # columns
+            origin=(np.random.random(), np.random.random(), 0.0),
+            dxyz_dij=np.array([[1.0, 0.0, 0.0], [0.0, 0.5, 0.0]]),
+            z_values=z_values,
+            title="Random test-data",
+            originator="someon",
+        )
+        mesh.create_xml()
+        print(model.roots()[0])
+        # Check if we get the correct xml-document out
+        print(ET.tostring(model.roots()[1]))
+        # TODO: Check if we need to handle the external part reference manually
+        # This might be handled by the server when it creates its own hdf5-file
+        # print(model.external_parts_list())
 
-            if (mh_record["messageFlags"] & MHFlags.FIN.value) != 0:
-                # We have received a FIN-bit, i.e., the last reponse has been
-                # read.
-                print("Last message read from this protocol")
-                break
+        # Here 'dataspace' exists
+        await close_session(ws, "🙉")
+        wat
 
         # Get resources
         mh_record = dict(
             protocol=3,  # Store
             messageType=1,  # GetResources
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,
         )
         gr_record = dict(  # GetResources
@@ -331,7 +509,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             protocol=4,  # Store
             messageType=1,  # GetDataObjects
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # Multi-part=False
         )
         gdo_record = dict(
@@ -406,7 +584,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             protocol=9,  # DataArray
             messageType=6,  # GetDataArrayMetadata
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # Multi-part=False
         )
         gdam_record = dict(
@@ -474,7 +652,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             protocol=9,  # DataArray
             messageType=2,  # GetDataArrays
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # Multi-part=False
         )
         gda_record = dict(
@@ -549,7 +727,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             protocol=9,  # DataArray
             messageType=3,  # GetDataSubrrays
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # Multi-part=False
         )
         gds_record = dict(
@@ -622,7 +800,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
             protocol=9,  # DataArray
             messageType=3,  # GetDataSubrrays
             correlationId=0,  # Ignored
-            messageId=await msg_id(),
+            messageId=await ClientMessageId.get_next_id(),
             messageFlags=MHFlags.FIN.value,  # Multi-part=False
         )
         gds_record = dict(
@@ -693,32 +871,7 @@ async def start_and_stop(url="ws://localhost:9002", headers={}):
         assert subdata_rec.shape == data.shape
         np.testing.assert_allclose(subdata_rec, data)
 
-        # Close session
-        mh_record = dict(
-            protocol=0,  # Core
-            messageType=5,  # CloseSession
-            correlationId=0,  # Ignored
-            messageId=await msg_id(),
-            messageFlags=MHFlags.FIN.value,
-        )
-
-        cs_record = dict(
-            reason="Because I said so",
-        )
-
-        fo = io.BytesIO()
-        fastavro.write.schemaless_writer(
-            fo, etp_schemas["Energistics.Etp.v12.Datatypes.MessageHeader"], mh_record
-        )
-        fastavro.write.schemaless_writer(
-            fo, etp_schemas["Energistics.Etp.v12.Protocol.Core.CloseSession"], cs_record
-        )
-
-        await ws.send(fo.getvalue())
-
-        await ws.wait_closed()
-        assert ws.closed
-        print(f"Websocket close reason: {ws.close_reason}")
+        await close_session(ws, "We are done 💅")
 
 
 # TODO: Populate localhost with data
